c12d4ba0f55320032a8d3dc43794caaa7be05a62
sobolevn <mail@sobolevn.me>
2019-10-20 16:35:14 +0300

    First release

--- tests/test_callable/test_getitem.py
+++ tests/test_callable/test_getitem.py
@@ -6,4 +6,4 @@ from lambdas import _
 def test_getitem():
     """Ensures that getitem works correctly."""
     assert _['a']({'a': 1}) + 2 == 3
-    assert _[0]({0: 'a'}) + 'b' == 'ab'
+    assert _[0]({0: 'a'}) + 'b' == 'ab'  # noqa: WPS336
