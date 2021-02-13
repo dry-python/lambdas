586a1291bafa1a2cb99f5a6be9be3ac487619471
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2021-02-13 01:43:12 -0300

    Fixes tests

--- tests/test_math_expression/test_truediv_expression.py
+++ tests/test_math_expression/test_truediv_expression.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import math
+
 from lambdas import _MathExpression
 
 
 def test_truediv():
     """Ensures that add works correctly."""
     truediv = _MathExpression() / 2
-    assert truediv(9) == 4.5
+    assert math.isclose(truediv(9), 4.5)  # type: ignore
 
 
 def test_rtruediv():
